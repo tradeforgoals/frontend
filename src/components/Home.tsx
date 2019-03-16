1b20e402a8bcb4179c8d6d8ee61cf8d26e13ca89
Marc <marcveens@hotmail.com>
2019-03-16 15:52:09 +0100

    Travis docker update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home: React.SFC = () => {
   return (
     <div>
-      <h1>Welcome to Trade For Goals!</h1>
+      <h1>Welcome to Trade For Goals!!</h1>
     </div>
   );
 };
