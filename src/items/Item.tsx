4a6f856db4570ebde2517556707f01a1a2f0fe5c
Thomas Jüsche <thomasjusche@sterrkdeta.nl>
2019-04-23 16:41:53 +0200

    Transform item into renderprop function

--- src/items/Item.tsx
+++ src/items/Item.tsx
@@ -1,55 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Box, Text } from 'grommet';
 
-import { Item as ItemInterface } from './Items';
 import Card from './Card';
-import axios from '../axios';
-import { RouteComponentProps } from 'react-router';
+import WithItem, { ItemProps } from './WithItem';
 
-export interface ItemState {
-  item: ItemInterface | null;
-  error: string | null;
-  loading: boolean;
-}
-
-class Item extends Component<RouteComponentProps<{ id: string }>> {
-  public state: ItemState = {
-    error: null,
-    loading: false,
-    item: null
-  };
-
-  public componentDidMount = async () => {
-    this.setState({ loading: true });
-    const id = this.props.match.params.id;
-    const response = await axios.get(`/items/${id}`);
-    this.setState({ loading: false, error: null, item: response.data });
-  };
-
-  public render() {
-    const { error, loading, item } = this.state;
-    let title = '';
-    if (loading) {
-      title = 'Loading...';
-    }
-    if (item) {
-      title = item.title;
-    }
-    if (error) {
-      return (
-        <Box direction="row" wrap justify="center">
-          <Text>{error}</Text>
+const Item = () => {
+  return (
+    <WithItem>
+      {({ error, loading, item }: ItemProps) => (
+        <Box>
+          <h1>{item ? item.title : ''}</h1>
+          {loading && <Text>Loading...</Text>}
+          {error && <Text>{error}</Text>}
+          {item && <Card {...item} />}
         </Box>
-      );
-    }
-    return (
-      <div>
-        <h1>{title}</h1>
-        {error && <Text>{error}</Text>}
-        {item && <Card {...item} />}
-      </div>
-    );
-  }
-}
+      )}
+    </WithItem>
+  );
+};
 
 export default Item;
---
+++ src/items/WithItem.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { RouteComponentProps, withRouter } from 'react-router';
+import axios from '../axios';
+import { Item } from './Items';
+
+export interface ItemProps {
+  error: string | null;
+  loading: boolean;
+  item?: Item;
+}
+
+class WithItem extends Component<
+  RouteComponentProps<{ id: string }> & {
+    children(props: ItemProps): JSX.Element;
+  }
+> {
+  public state: ItemProps = {
+    error: null,
+    loading: false,
+    item: undefined
+  };
+
+  public componentDidMount = async () => {
+    this.setState({ loading: true });
+    const id = this.props.match.params.id;
+    const response = await axios.get(`/items/${id}`);
+    this.setState({ loading: false, error: null, item: response.data });
+  }
+
+  public render() {
+    const { children } = this.props;
+    const { error, loading, item } = this.state;
+    return children({ error, loading, item });
+  }
+}
+export default withRouter(WithItem);
